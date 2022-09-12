951c26e235f6e699ea415a0f591e17b45d6efed5
frqhero <frqhero@gmail.com>
2022-09-12 18:18:05 +0300

    idk

--- cats/views.py
+++ cats/views.py
@@ -3,14 +3,24 @@ from rest_framework import viewsets
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .permissions import OwnerOrReadOnly, ReadOnly
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,) 
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
+
+    # def get_permissions(self):
+    # # Если в GET-запросе требуется получить информацию об объекте
+    #     if self.action == 'retrieve':
+    #         # Вернем обновленный перечень используемых пермишенов
+    #         return (ReadOnly(),)
+    #     # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+    #     return super().get_permissions() 
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
